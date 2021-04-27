9e0ea421e045b73826bfa5cfb9e43b26b4bfe25b
James Caruana <caruana.james22@gmail.com>
2021-04-27 16:38:08 +0200

    Minor tweaks
    Correctly setup the application

--- cmd/root.go
+++ cmd/root.go
@@ -29,13 +29,12 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go_quiz",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A simple quiz",
+	Long: `Golang quiz which requires to fill the following requirements:
+	- Create a multiple choice quiz (get data from an quiz API).
+	- User can select one answer only.
+	- Display how many correct answers user got.
+	- Show some statistics.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
